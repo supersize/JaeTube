dcbecf25eec39a4ac260c322ee8609a5dd664d3e
KimJaeHyeong <kimjaehyeong92@gmail.com>
2022-11-25 13:21:21 +0800

    prod.js upload

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : process.env.MONGO_URI // heroku에 db 정보 설정 위한 코드. 난 aws에 할건데 방법 찾아 봐야 할듯.
+    // mongoURI : process.env.MONGO_URI // heroku에 db 정보 설정 위한 코드. 난 aws에 할건데 방법 찾아 봐야 할듯.
+    mongoURI : 'mongodb+srv://wogud541:qlraocl12@boilerplate.4c95e.mongodb.net/?retryWrites=true&w=majority'
 }
