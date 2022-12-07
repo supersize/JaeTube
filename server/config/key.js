f78562a8bbb9166467ef4e44d6bcf0412eda2364
KimJaeHyeong <kimjaehyeong92@gmail.com>
2022-12-07 09:49:15 +0800

    log added

--- server/config/key.js
+++ server/config/key.js
@@ -1,7 +1,8 @@
 // console.log("this is key");
 // console.log("process.env : ", process.env);
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod');
+    module.exports = require('./prod.js');
+    console.log("process.env.mongoURI in KEY : ", process.env.mongoURI);
 } else {
-    module.exports = require('./dev');
+    module.exports = require('./dev.js');
 }
