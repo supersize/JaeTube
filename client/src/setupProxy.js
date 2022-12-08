3351c6cc61be2066a4cb77dcc6f50673300b2384
KimJaeHyeong <kimjaehyeong92@gmail.com>
2022-12-08 11:35:05 +0800

    port test

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://18.117.21.81/',
+      target: 'http://18.117.21.81:80/',
       secure: false,
       changeOrigin: true,
     })
