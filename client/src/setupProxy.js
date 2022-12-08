61e3d2d6b8d1b24f14f1e2882dd41a012cfbf9e4
KimJaeHyeong <kimjaehyeong92@gmail.com>
2022-12-08 11:16:55 +0800

    setupProxy address changed

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: 'http://18.117.21.81:3001/',
       secure: false,
       changeOrigin: true,
     })
