953d5c3576313efe405f7ef4c6cadd752ba9e1fa
KimJaeHyeong <kimjaehyeong92@gmail.com>
2023-06-04 17:57:36 +0800

    asdf

---
+++ client/build/precache-manifest.ef94d3035e41cd76930edfae88a9720b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a96784364de748dd8ddd9ca7fa4400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82828cbe4ca8bece45c3",
+    "url": "/static/css/2.52c6b7cb.chunk.css"
+  },
+  {
+    "revision": "62084dba2f852331f7b0",
+    "url": "/static/css/main.4ce5d972.chunk.css"
+  },
+  {
+    "revision": "82828cbe4ca8bece45c3",
+    "url": "/static/js/2.7b8473a9.chunk.js"
+  },
+  {
+    "revision": "98be86e4f2744ed590c9dea62ca7a8a3",
+    "url": "/static/js/2.7b8473a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62084dba2f852331f7b0",
+    "url": "/static/js/main.8f9b41df.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.8f9b41df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
